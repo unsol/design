70fd5567c4b13aa36553293e4ad9e7b636febcac
wanderer <mjbecze@gmail.com>
2016-03-08 15:52:18 -0500

    added address test

--- js-prototype/test_runner.js
+++ js-prototype/test_runner.js
@@ -1,3 +1,4 @@
+'use strict'
 const tape = require('tape')
 const fs = require('fs')
 const exec = require('child_process').exec
@@ -6,10 +7,13 @@ const exec = require('child_process').exec
 const tests = fs.readdirSync('../tests').filter((file) => file.split('.')[1] === 'wast')
 
 // run the tests
-for (const testName in tests) {
+for (let testName of tests) {
+  testName = testName.split('.')[0]
   tape(testName, (t) => {
     // Compile Command
     const cmpCmd = `../sexpr-wasm-prototype/out/sexpr-wasm ./tests/${testName}.wast -o ./tests/${testName}.wasm`
+
+    console.log(cmpCmd)
     // Run Command
     const runCmd = `d8 -e "const testName = '${testName}' --expose-wasm interface.js tester.js`
 

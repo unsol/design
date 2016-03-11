62ac2cd7dbdfa6625a189d97fed5b87a41b0d1f9
wanderer <mjbecze@gmail.com>
2016-03-11 15:17:22 -0500

    added sexp submodule

--- js-prototype/testEnviroment.js
+++ js-prototype/testEnviroment.js
@@ -45,6 +45,10 @@ class Enviroment {
     if (data.callValue) {
       this.callValue = new Uint8Array(data.callValue)
     }
+
+    if (data.callData) {
+      this.callData = hexStr2arrayBuf(data.callData)
+    }
   }
 
   getBalance (address) {
@@ -72,4 +76,19 @@ class Enviroment {
     // STUB
     return // result
   }
+}
+
+function hexStr2arrayBuf (string) {
+  const ab = new ArrayBuffer(string.length / 2)
+  const view = new Uint8Array(ab)
+  string = [...string]
+  let temp = ''
+  string.forEach((el, i) => {
+    temp += el
+    if (i % 2) {
+      view[(i + 1) / 2 - 1] = parseInt(temp, 16)
+      temp = ''
+    }
+  })
+  return ab
 }

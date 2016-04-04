771775025163a2260362dd67f57dea14335eb651
wanderer <mjbecze@gmail.com>
2016-04-04 12:32:54 -0400

    added assests

--- metering-prototype/bin/injectMetering.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict'
-
-const fs = require('fs')
-const injector = require('../')
-const argv = require('yargs').argv
-const wastFile = fs.readFileSync(argv._[0])
-
-process.stdout.write(injector.injectWAST(wastFile))
